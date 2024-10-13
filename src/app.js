14d3b6506162f456ad24d8544685f838210b8ffb
iam-abin <iamabin1100@gmail.com>
2024-10-13 21:39:18 +0530

    feat(user): add feed api

--- src/app.js
+++ src/app.js
@@ -17,12 +17,15 @@ app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ extended: true, limit: "30mb" }));
 app.use(cors({ origin: "*" }));
 app.use(morgan("dev"));
-app.use(cookieParser())
+app.use(cookieParser());
 
 // ROUTES
 app.use(`${BASE_URL}/auth`, authRoute);
 app.use(`${BASE_URL}/profile`, profileRoute);
 app.use(`${BASE_URL}/request`, requestRoute); // routes for connection requests
 app.use(`${BASE_URL}/user`, userRoute);
+app.all("*", (req, res) => {
+    res.status(404).json({ error: true, message: "Route not found" });
+});
 
 export { app };
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { authUser } from "../middlewares/auth.js";
 import ConnectionRequestModel from "../models/connectionRequest.js";
+import UserModel from "../models/user.js";
 
 const router = express.Router();
-const USER_SAFE_FIELDS = [
+
+const USER_SAFE_DATA_FIELDS = [
     "firstName",
     "lastName",
     "photoUrl",
@@ -22,7 +24,7 @@ router.get("/requests/received", authUser, async (req, res) => {
             status: "interested",
         })
             .select(["fromUserId", "createdAt"])
-            .populate("fromUserId", USER_SAFE_FIELDS);
+            .populate("fromUserId", USER_SAFE_DATA_FIELDS);
 
         res.json({
             message: "Connect requests fetched successfully",
@@ -44,12 +46,15 @@ router.get("/connections", authUser, async (req, res) => {
             ],
         })
             // .select(["fromUserId", "createdAt"])
-            .populate("fromUserId", USER_SAFE_FIELDS)
-            .populate("toUserId", USER_SAFE_FIELDS);
+            .populate("fromUserId", USER_SAFE_DATA_FIELDS)
+            .populate("toUserId", USER_SAFE_DATA_FIELDS);
 
         // To remove the details of current loggened user from the result data
         const data = connections.map((connection) => {
-            if (connection.fromUserId._id.toString() === currentUser._id.toString()) {
+            if (
+                connection.fromUserId._id.toString() ===
+                currentUser._id.toString()
+            ) {
                 return connection.toUserId;
             }
             return connection.fromUserId;
@@ -61,6 +66,47 @@ router.get("/connections", authUser, async (req, res) => {
         });
     } catch (error) {
         res.status(500).json({ error: true, message: error.message });
+    }
+});
+
+router.get("/feed", authUser, async (req, res) => {
+    try {
+        const currentUser = req.user;
+
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page - 1) * limit;
+
+        const connectionRequests = await ConnectionRequestModel.find({
+            $or: [
+                { toUserId: currentUser._id },
+                { fromUserId: currentUser._id },
+            ],
+        }).select("fromUserId  toUserId");
+
+        const hideUsersFromFeed = new Set();
+        connectionRequests.forEach((request) => {
+            hideUsersFromFeed.add(request.fromUserId.toString());
+            hideUsersFromFeed.add(request.toUserId.toString());
+        });
+
+        const users = await UserModel.find({
+            $and: [
+                { _id: { $nin: Array.from(hideUsersFromFeed) } },
+                { _id: { $ne: currentUser._id } },
+            ],
+        })
+            .select(USER_SAFE_DATA_FIELDS)
+            .skip(skip)
+            .limit(limit);
+
+        res.status(200).json({
+            message: "feed data fetched successfully",
+            data: users,
+        });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
     }
 });
 
